9b253d44d015d12ac7c4e2e2d277b8c690b3e43f
stark-t <thomasstark@live.de>
2022-06-28 13:42:51 +0200

    TS: auto download yolov5 if not exists

--- scripts/run_yolov5_train.py
+++ scripts/run_yolov5_train.py
@@ -1,5 +1,6 @@
 # import packages
 import os
+from git import Repo
 
 # import scripts
 import utils_config as config
@@ -16,13 +17,21 @@ if __name__ == '__main__':
     hyperparms = r'C:\Users\star_th\PycharmProjects\PAI\code\yolov5\data\hyps\hyp.scratch-med.yaml'
     save_dir = r'F:\202105_PAI\data\P1_yolov5'
 
-    # Maybe check if yolo is there?
-
+    # check if yolo needs to be downloaded
+    # get current dir
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, 'relative/path/to/file/you/want')
+    # split and create yolov5 dir
+    dirname_splits = os.path.normpath(dirname).split(os.path.sep)[0:-1]
+    drive_path = dirname_splits[0] + os.sep
+    detectors_path = os.path.join(drive_path, *dirname_splits[1:])
+    detectors_path_yolov5 = os.path.join(detectors_path, 'detectors', 'yolov5')
+    detectors_path_yolov5_trainpy = os.path.join(detectors_path, 'train.py')
+    # check if train.py exists
+    if not os.path.isfile(detectors_path_yolov5_trainpy):
+        print('download yolov5 into /PAI/detectors/yolov5')
+        print('download yolov5 from github')
+        Repo.clone_from('https://github.com/ultralytics/yolov5.git', detectors_path)
 
-    if not os.path.exists(r'.code\yolov5\train.py'):
-        d=1
 
     train.run(data=data_yaml, weights=weights, batch_size=batch_size,
               epochs=epochs, imgsz=image_size,hyp=hyperparms, project=save_dir)
