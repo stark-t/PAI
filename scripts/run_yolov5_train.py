a62d742aacc0ad308c333a670ca828b410518adb
stark-t <thomasstark@live.de>
2022-06-29 09:09:10 +0200

    TS: auto download yolov5

--- scripts/run_yolov5_train.py
+++ scripts/run_yolov5_train.py
@@ -4,7 +4,7 @@ from git import Repo
 
 # import scripts
 import utils_config as config
-from detectors.yolov5 import train
+
 
 
 if __name__ == '__main__':
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     dirname_splits = os.path.normpath(dirname).split(os.path.sep)[0:-1]
     drive_path = dirname_splits[0] + os.sep
     detectors_path = os.path.join(drive_path, *dirname_splits[1:])
-    detectors_path_yolov5 = os.path.join(detectors_path, 'detectors', 'yolov5')
+    detectors_path = os.path.join(detectors_path, 'detectors', 'yolov5')
     detectors_path_yolov5_trainpy = os.path.join(detectors_path, 'train.py')
     # check if train.py exists
     if not os.path.isfile(detectors_path_yolov5_trainpy):
@@ -32,7 +32,12 @@ if __name__ == '__main__':
         print('download yolov5 from github')
         Repo.clone_from('https://github.com/ultralytics/yolov5.git', detectors_path)
 
+    from detectors.yolov5 import train
 
+    # check if dataset exists
+
+
+    # run yolov5
     train.run(data=data_yaml, weights=weights, batch_size=batch_size,
               epochs=epochs, imgsz=image_size,hyp=hyperparms, project=save_dir)
 
