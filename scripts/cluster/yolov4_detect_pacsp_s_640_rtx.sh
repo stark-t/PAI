5ed71175c8be2984f47abdcb89029e0671463bb3
valentinitnelav <stefan.valentin87@yahoo.com>
2022-09-02 14:10:38 +0200

    See issues #51 #52 & #53

--- scripts/cluster/yolov4_detect_pacsp_s_640_rtx.sh
+++ scripts/cluster/yolov4_detect_pacsp_s_640_rtx.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=4 # request number of CPUs
 #SBATCH --gres=gpu:rtx2080ti:1 # type and number of requested GPUs; Options are rtx2080ti:1 or gpu:v100:1
 #SBATCH --mem-per-gpu=11G # RAM per GPU - 11 Gb is for NVIDIA GeForce RTX 2080 Ti; 32 Gb for Nvidia Tesla V100 
-#SBATCH --time=01:00:00 # requested time in d-hh:mm:ss e.g. 10-00:00:00 = 10 days, 100:00:00 = 100 hours; 00:30:00 = 30 min
+#SBATCH --time=20:00:00 # requested time in d-hh:mm:ss e.g. 10-00:00:00 = 10 days, 100:00:00 = 100 hours; 00:30:00 = 30 min
 #SBATCH --output=/home/sc.uni-leipzig.de/sv127qyji/PAI/detectors/logs_detect_jobs/%j.log # path for job-id.log file;
 #SBATCH --error=/home/sc.uni-leipzig.de/sv127qyji/PAI/detectors/logs_detect_jobs/%j.err # path for job-id.err file;
 #SBATCH --mail-type=BEGIN,TIME_LIMIT,END # email options
@@ -26,23 +26,37 @@ source ~/PAI/scripts/cluster/session_info.sh PyTorch_YOLOv4
 
 cd ~/PAI/detectors/PyTorch_YOLOv4
 
-# See https://github.com/WongKinYiu/PyTorch_YOLOv4/blob/master/detect.py
-python detect.py \
---weights ~/PAI/detectors/PyTorch_YOLOv4/runs/train/yolov4_pacsp_s_b8_e300_img640_hyp_custom/weights/best_overall.pt \
---source /home/sc.uni-leipzig.de/sv127qyji/datasets/P1_Data_sampled/test/images \
---img-size 640 \
---conf-thres 0.25 \
---iou-thres 0.45 \
---save-txt \
---cfg ~/PAI/detectors/PyTorch_YOLOv4/cfg/yolov4-csp-s-leaky.cfg \
---names ~/PAI/detectors/PyTorch_YOLOv4/data/pai.names \
---output runs/detect/"$SLURM_JOB_ID"_detection_using_3217130_yolov4_pacsp_s_b8_e300_img640_hyp_custom
+# This will create a folder in  ~/PAI/detectors/PyTorch_YOLOv4/runs/detect with the name given in the --output argument,
+# job_"$SLURM_JOB_ID"_loop_detect_on_3217130_yolov4_pacsp_s_b8_e300_img640_hyp_custom
+# In that folder will create several other folders containing the label & images files.
+# The names of these folders correspond to different conf and IoU levels.
 
+for conf in $(seq 0.1 0.1 0.9)
+do
+    for iou in $(seq 0.1 0.1 0.9)
+    do
+        # See https://github.com/WongKinYiu/PyTorch_YOLOv4/blob/master/detect.py
+        python detect.py \
+        --weights ~/PAI/detectors/PyTorch_YOLOv4/runs/train/yolov4_pacsp_s_b8_e300_img640_hyp_custom/weights/best.pt \
+        --source /home/sc.uni-leipzig.de/sv127qyji/datasets/P1_Data_sampled/test/images \
+        --img-size 640 \
+        --conf-thres "$conf" \
+        --iou-thres "$iou" \
+        --save-txt \
+        --cfg ~/PAI/detectors/PyTorch_YOLOv4/cfg/yolov4-csp-s-leaky.cfg \
+        --names ~/PAI/detectors/PyTorch_YOLOv4/data/pai.names \
+        --output runs/detect/job_"$SLURM_JOB_ID"_loop_detect_on_3217130_yolov4_pacsp_s_b8_e300_img640_hyp_custom/results_at_conf_"$conf"_iou_"$iou"
+    done
+done
 # Note that, PyTorch_YOLOv4 doesn't have:
 # --max-det
 # --save-conf
 # --nosave
 # --name - use --output instead
+# --project, use --output instead
+# Similar to yolov7, it also doesn't have --max-det argument as yolov5
+
+# For --output, need to create a parent folder, similar to what the --project argument from YOLOv5 & 7 does.
 
 # Deactivate virtual environment
 deactivate
--- scripts/cluster/yolov7_detect_640_rtx.sh
+++ scripts/cluster/yolov7_detect_640_rtx.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=4 # request number of CPUs
 #SBATCH --gres=gpu:rtx2080ti:1 # type and number of requested GPUs; Options are rtx2080ti:1 or gpu:v100:1
 #SBATCH --mem-per-gpu=11G # RAM per GPU - 11 Gb is for NVIDIA GeForce RTX 2080 Ti; 32 Gb for Nvidia Tesla V100 
-#SBATCH --time=01:00:00 # requested time in d-hh:mm:ss e.g. 10-00:00:00 = 10 days, 100:00:00 = 100 hours; 00:30:00 = 30 min
+#SBATCH --time=20:00:00 # requested time in d-hh:mm:ss e.g. 10-00:00:00 = 10 days, 100:00:00 = 100 hours; 00:30:00 = 30 min
 #SBATCH --output=/home/sc.uni-leipzig.de/sv127qyji/PAI/detectors/logs_detect_jobs/%j.log # path for job-id.log file;
 #SBATCH --error=/home/sc.uni-leipzig.de/sv127qyji/PAI/detectors/logs_detect_jobs/%j.err # path for job-id.err file;
 #SBATCH --mail-type=BEGIN,TIME_LIMIT,END # email options
@@ -26,16 +26,26 @@ source ~/PAI/scripts/cluster/session_info.sh yolov7
 
 cd ~/PAI/detectors/yolov7
 
-python detect.py \
---weights ~/PAI/detectors/yolov7/runs/train/3219805_yolov7_img640_b8_e300_hyp_custom/weights/best.pt \
---source /home/sc.uni-leipzig.de/sv127qyji/datasets/P1_Data_sampled/test/images \
---img-size 640 \
---conf-thres 0.25 \
---iou-thres 0.45 \
---save-txt \
---save-conf \
---nosave \
---name "$SLURM_JOB_ID"_detection_using_3219805_yolov7_img640_b8_e300_hyp_custom
+# This will create a folder in ~/PAI/detectors/yolov7/runs/detect with the name given in the --project argument.
+# In that folder will create several other folders containing the label files.
+# The names of these folders are created via the --name argument. They correspond to different conf and IoU levels.
+for conf in $(seq 0.1 0.1 0.9)
+do
+    for iou in $(seq 0.1 0.1 0.9)
+    do
+        python detect.py \
+        --weights ~/PAI/detectors/yolov7/runs/train/3219805_yolov7_img640_b8_e300_hyp_custom/weights/best.pt \
+        --source /home/sc.uni-leipzig.de/sv127qyji/datasets/P1_Data_sampled/test/images \
+        --img-size 640 \
+        --conf-thres "$conf" \
+        --iou-thres "$iou" \
+        --save-txt \
+        --save-conf \
+        --nosave \
+        --project runs/detect/job_"$SLURM_JOB_ID"_loop_detect_on_3219805_yolov7_img640_b8_e300_hyp_custom \
+        --name results_at_conf_"$conf"_iou_"$iou"
+    done
+done
 
 # Note that, yolov7 doesn't have --max-det argument as yolov5
 
